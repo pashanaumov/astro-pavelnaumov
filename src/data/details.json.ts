464b30ef266fcb018e1f7197738abb0962116f07
Pavel Naumov <pavliknaumov@gmail.com>
2023-06-09 10:48:30 +0300

    feat: add company name to info

--- src/data/details.json.ts
+++ src/data/details.json.ts
@@ -1,34 +1,41 @@
 export interface Template {
+  company: string;
   title: string;
   description: string;
   date: string;
 }
+
 const one: Template = {
+  company: "eApteka",
   title: "Senior software developer",
   description:
-    "Tasked with developing a mobile application for Russia’s largest e-pharmacy.  Involved in the full software development cycle. Leading a team of four people.",
+    "Tasked with developing a mobile application for a large e-pharmacy.  Involved in the full software development cycle. Leading a team of four people.",
   date: "Sep 2021 - ",
 };
 const two: Template = {
+  company: "IT•BP",
   title: "Software developer",
   description:
-    "Developed and maintained a data-exchange platform for a large Russian logistics conglomerate: corporate messenger, corporate info portal, document exchange.",
+    "Developed and maintained a data-exchange platform for a large logistics conglomerate: corporate messenger, corporate info portal, document exchange.",
   date: "Aug 2019 - Sep 2021",
 };
 const three: Template = {
+  company: "JS+",
   title: "CTO",
   description:
     "Co-founded a start-up that helps doctors and medical organizations to protect themselves from potential breaches of the law and consumer extremisms.",
   date: "Jan 2019 - Mar 2021",
 };
 const four = {
+  company: "",
   title: "Freelancer",
   description:
-    "Started professional career as a developer by developing small e-commerce website using Ruby on Rails. Worked in a couple of projects using Angular 2.",
+    "Started professional career as a developer by developing small e-commerce websites using Ruby on Rails. Worked in a couple of projects using Angular 2.",
   date: "Aug 2018 - Dec 2018",
 };
 
 const five = {
+  company: "Takk",
   title: "Co-founder",
   description:
     "Co-founded a startup that aimed to help SME's hire the best local professionals in order to increase their efficiency and reduce costs.",
